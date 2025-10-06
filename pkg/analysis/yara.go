35a0b042dad43ea49ba05a5ed380e8abf3bc9197
wellatleastitried <jackson1swindell@gmail.com>
2025-10-05 23:06:47 -0500

    Changed package structure for analysis

--- pkg/analysis/yara.go
+++ pkg/analysis/yara.go
@@ -1,10 +1,22 @@
-package malware
+package analysis
 
 import (
     "fmt"
 
     //"github.com/hillu/go-yara/v4"
 )
+
+type Match struct {
+    Rule string
+    Namespace string
+    Tags []string
+    Strings []string
+}
+
+type Result struct {
+    Matches []Match
+    Error error
+}
 
 // Use Yara rules to detect malware signatures
 func ImportYaraRules() {
